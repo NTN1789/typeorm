fc0bcf1710d65fc2c6d3c71cba4333fdcb7cbc17
Natan <natanalmeida040@gmail.com>
2024-07-18 14:22:49 -0300

    teste

--- typeOrm/typeorms/src/app.module.ts
+++ typeOrm/typeorms/src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module, forwardRef , } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.Module';
-import {ThrottlerGuard, ThrottlerModule} from  '@nestjs/throttler'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-import {TypeOrmModule} from "@nestjs/typeorm"
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user/entity/user.entinty';
 @Module({
   imports: [
@@ -15,7 +15,7 @@ import { UserEntity } from './user/entity/user.entinty';
       ttl: 60,
       limit: 100,
     }),
-    forwardRef(() => UserModule),   
+    forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
 
     TypeOrmModule.forRoot({
@@ -25,15 +25,18 @@ import { UserEntity } from './user/entity/user.entinty';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities:[UserEntity],
-      synchronize: process.env.ENV === "development"   , 
-    })
+      entities: [UserEntity],
+      synchronize: process.env.ENV === 'development',
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService , {
-    provide: APP_GUARD,
-    useClass:ThrottlerGuard
-  }],
-  exports:[AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+  exports: [AppService],
 })
 export class AppModule {}
--- typeOrm/typeorms/src/auth/guards/auth.guard.ts
+++ typeOrm/typeorms/src/auth/guards/auth.guard.ts
@@ -1,42 +1,44 @@
-import { CanActivate, ExecutionContext, Inject, Injectable, forwardRef } from "@nestjs/common";
-import { AuthService } from "../auth.service";
-import { UserService } from "src/user/user.service";
-
-
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  forwardRef,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate{
-                   // o CanActivate e importante dms nos guards                              
+export class AuthGuard implements CanActivate {
+  // o CanActivate e importante dms nos guards
 
-        constructor(
+  constructor(
+   
+    private readonly authService: AuthService, 
+    private readonly userService: UserService,
+  ) {}
 
-             //  @Inject(forwardRef(() => AuthService)) // esse metodo daria certo , só que teria que fazer em todas as depêndecias do projeto 
-            private readonly authService: AuthService , // verificando o token é pegando os dados 
-            private readonly userService: UserService
-            
-            ){}
+  async canActivate(context: ExecutionContext) {
+    // executionContext e especial do nestj/common
 
-       async  canActivate(context: ExecutionContext){
-                                    // executionContext e especial do nestj/common
+    const request = context.switchToHttp().getRequest();
 
-          const request =  context.switchToHttp().getRequest();
-          
-          const {authorization} = request.headers;
-         
-            try {
-                const data =   this.authService.checkToken((authorization ?? '').split(' ')[1]); 
-                
-                request.tokenPayload = data;
+    const { authorization } = request.headers;
 
-               request.user =   await this.userService.show(data.id)
+    try {
+      const data = this.authService.checkToken(
+        (authorization ?? '').split(' ')[1],
+      );
 
-                return true;
+      request.tokenPayload = data;
 
-            } catch (error) {
-                return false;  
-            }
-            
-                // pegando os daods do token            
-      
-        }
+      request.user = await this.userService.show(data.id);
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+
+    // pegando os daods do token
+  }
 }
